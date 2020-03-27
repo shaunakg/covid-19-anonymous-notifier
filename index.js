2665982bfe03979b58f090d2af8f6ec4ee2dc021
shaunakg <gadkari.shaunak@gmail.com>
2020-03-27 14:32:10 +1100

    cleaned code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config({path:'sendgrid.env'});
 var fs = require('fs');
 var notifHtml = fs.readFileSync(__dirname + '/static/notif-html.html', 'utf8');
 var notifTextOnly = fs.readFileSync(__dirname + '/static/notif.txt', 'utf8');
+var to_user = "";
 
 console.log(process.env.SENDGRID_API_KEY);
 
@@ -13,22 +14,13 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-	to: 'gadkari.shaunak@gmail.com',
+	to: to_user,
 	name: 'Shaunak Gadkari',
 	from: 'notification@covid-anonymous.shaunakg.me',
 	subject: 'Important! Someone you were in contact with recently tested positive to COVID-19',
 	text: notifTextOnly,
 	html: notifHtml,
 };
-
-// const msg = {
-// 	to: 'gadkari.shaunak@gmail.com',
-// 	name: 'Shaunak Gadkari',
-// 	from: 'notification@covid-anonymous.shaunakg.me',
-// 	subject: 'This is a sendgrid test message!',
-// 	text: "Test!",
-// 	html: "HTML!",
-// };
 
 sgMail
 	.send(msg)
